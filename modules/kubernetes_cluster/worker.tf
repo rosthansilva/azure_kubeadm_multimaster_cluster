91c6cdd3c47cd8ca198e2dbd5cd7765c901e005f
Rosthan Silva <rosthan.silva@sinqia.com.br>
2022-04-09 00:08:51 -0300

    reestruturando Modulo

--- modules/kubernetes_cluster/worker.tf
+++ modules/kubernetes_cluster/worker.tf
@@ -4,18 +4,16 @@ resource "azurerm_virtual_machine" "workers" {
   count                 = var.worker_count
   name                  = "${var.worker_name}-${var.resource_group}-${var.location}-${count.index + 1}"
   location              = var.location
-  resource_group_name   = var.resource_group
+  resource_group_name   = var.resource_group    
+  size                  = "Standard_DS1_v2"
+  admin_username        = var.ssh_user
   network_interface_ids = element(local.vm_nics_worker, count.index + 1)
-   admin_username        = var.ssh_user    
-  vm_size               = "Standard_DS1_v2"
-
-  delete_os_disk_on_termination = true
-  delete_data_disks_on_termination = true
 
   admin_ssh_key {
     username = var.ssh_user
     public_key = file("~/.ssh/id_rsa.pub")
   }
+  
   storage_image_reference {
     publisher = "credativ"
     offer     = "Debian"
--- variables.tf
+++ variables.tf
@@ -1,32 +1,12 @@
-variable "location" {
-  description = "The resource group location"
-  default     = "West Europe"
-}
-
-variable "kube_vnet_name" {
-    default = "kube-cluser-vnet-net"
-}
-
-variable "hub_vnet_name" {
-    default = "hub-vnet"
-}
-
-variable "ssh_user" {
-  type = string
-  default = "htd"
-}
-
-variable "ssh_key" {
-}
 
 variable "default_tags" {
-  type = map
+  type        = map(any)
   description = "Map of Default Tags"
   default = {
-    Administrator = "Rosthan Silva"
-    Department = "DevOps"
-    CostCentre = "HTD"
-    ContactPerson = "rosthan.silva@Sinqia.com.br"
+    Administrator      = "Rosthan Silva"
+    Department         = "DevOps"
+    CostCentre         = "HTD"
+    ContactPerson      = "rosthan.silva@Sinqia.com.br"
     ManagedByTerraform = "True"
   }
 }
