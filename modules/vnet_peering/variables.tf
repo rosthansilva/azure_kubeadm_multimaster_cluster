57bc16a97148c43636315b47938ed5a342a69d59
Rosthan Silva <rosthan.silva@sinqia.com.br>
2022-04-11 15:36:24 -0300

    #ADD - Modulos - VNET

--- modules/vnet_peering/variables.tf
+++ modules/vnet_peering/variables.tf
@@ -1,41 +1,41 @@
 variable "vnet_1_name" {
-  description = "VNET 1 name"
+  description = "Vnet 1 nome"
   type        = string
 }
 
 variable "vnet_1_id" {
-  description = "VNET 1 ID"
+  description ="VNET 1 ID"
   type        = string
 }
 
 variable "vnet_1_rg" {
-  description = "VNET 1 resource group"
+  description ="Grupo de recursos do VNET 1"
   type        = string
 }
 
 variable "vnet_2_name" {
-  description = "VNET 2 name"
+  description ="Nome do vnet 2"
   type        = string
 }
 
 variable "vnet_2_id" {
-  description = "VNET 2 ID"
+  description = "vnet2Id"
   type        = string
 }
 
 variable "vnet_2_rg" {
-  description = "VNET 2 resource group"
+  description = "Grupo de recursos do VNET 2"
   type        = string
 }
 
 variable "peering_name_1_to_2" {
-  description = "(optional) Peering 1 to 2 name"
+  description = "(opcional) olhando 1 a 2 nome"
   type        = string
   default     = "peering1to2"
 }
 
 variable "peering_name_2_to_1" {
-  description = "(optional) Peering 2 to 1 name"
+  description = "(opcional) olhando 2 a 1 nome"
   type        = string
   default     = "peering2to1"
 }
